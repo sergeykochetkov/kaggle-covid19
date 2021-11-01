44279b7cc6d8907aec5f210308744888988f67f6
sergeykochetkov <sergeykochetkov@yandex.ru>
2021-11-01 11:49:58 +0300

    unittests

--- src/classification_aux/generate_pseudo_label.sh
+++ src/classification_aux/generate_pseudo_label.sh
@@ -1,18 +1,24 @@
+cfg1=eb5_512_deeplabv3plus.yaml
+cfg2=eb6_448_linknet.yaml
+cfg3=eb7_512_unetplusplus.yaml
+cfg4=seresnet152d_512_unet.yaml
+folds=0
+
 #predict softlabel for public test
-CUDA_VISIBLE_DEVICES=0 python predict_test.py --cfg configs/eb5_512_deeplabv3plus.yaml --ckpt_dir $1
-CUDA_VISIBLE_DEVICES=0 python predict_test.py --cfg configs/eb6_448_linknet.yaml --ckpt_dir $1
-CUDA_VISIBLE_DEVICES=0 python predict_test.py --cfg configs/eb7_512_unetplusplus.yaml --ckpt_dir $1
-CUDA_VISIBLE_DEVICES=0 python predict_test.py --cfg configs/seresnet152d_512_unet.yaml --ckpt_dir $1
+CUDA_VISIBLE_DEVICES=0 python predict_test.py --cfg configs/$cfg1 --ckpt_dir $1 --folds $folds
+CUDA_VISIBLE_DEVICES=0 python predict_test.py --cfg configs/$cfg2 --ckpt_dir $1 --folds $folds
+CUDA_VISIBLE_DEVICES=0 python predict_test.py --cfg configs/$cfg3 --ckpt_dir $1 --folds $folds
+CUDA_VISIBLE_DEVICES=0 python predict_test.py --cfg configs/$cfg4 --ckpt_dir $1 --folds $folds
 #predict mask for public test using segmentation head
-CUDA_VISIBLE_DEVICES=0 python predict_test_seg.py --cfg configs/eb5_512_deeplabv3plus.yaml --ckpt_dir $1
-CUDA_VISIBLE_DEVICES=0 python predict_test_seg.py --cfg configs/eb6_448_linknet.yaml --ckpt_dir $1
-CUDA_VISIBLE_DEVICES=0 python predict_test_seg.py --cfg configs/eb7_512_unetplusplus.yaml --ckpt_dir $1
-CUDA_VISIBLE_DEVICES=0 python predict_test_seg.py --cfg configs/seresnet152d_512_unet.yaml --ckpt_dir $1
+CUDA_VISIBLE_DEVICES=0 python predict_test_seg.py --cfg configs/$cfg1 --ckpt_dir $1 --folds $folds
+CUDA_VISIBLE_DEVICES=0 python predict_test_seg.py --cfg configs/$cfg2 --ckpt_dir $1 --folds $folds
+CUDA_VISIBLE_DEVICES=0 python predict_test_seg.py --cfg configs/$cfg3 --ckpt_dir $1 --folds $folds
+CUDA_VISIBLE_DEVICES=0 python predict_test_seg.py --cfg configs/$cfg4 --ckpt_dir $1 --folds $folds
 #ensemble 4 models eb5, eb6, eb7, seresnet152
 python ensemble_pseudo_test.py
 #predict softlabel for padchest, pneumothorax, vin
-CUDA_VISIBLE_DEVICES=0 python predict_ext.py --cfg configs/eb5_512_deeplabv3plus.yaml --ckpt_dir $1
-CUDA_VISIBLE_DEVICES=0 python predict_ext.py --cfg configs/eb6_448_linknet.yaml --ckpt_dir $1
-CUDA_VISIBLE_DEVICES=0 python predict_ext.py --cfg configs/eb7_512_unetplusplus.yaml --ckpt_dir $1
-CUDA_VISIBLE_DEVICES=0 python predict_ext.py --cfg configs/seresnet152d_512_unet.yaml --ckpt_dir $1
+CUDA_VISIBLE_DEVICES=0 python predict_ext.py --cfg configs/$cfg1 --ckpt_dir $1 --folds $folds
+CUDA_VISIBLE_DEVICES=0 python predict_ext.py --cfg configs/$cfg2 --ckpt_dir $1 --folds $folds
+CUDA_VISIBLE_DEVICES=0 python predict_ext.py --cfg configs/$cfg3 --ckpt_dir $1 --folds $folds
+CUDA_VISIBLE_DEVICES=0 python predict_ext.py --cfg configs/$cfg4 --ckpt_dir $1 --folds $folds
 python ensemble_pseudo_ext.py
--- src/classification_aux/unittest_train.sh
+++ src/classification_aux/unittest_train.sh
@@ -6,6 +6,11 @@ FRAC=0.2
 
 CFG=configs/unittest_eb3_128_deeplabv3plus.yaml
 
+rm chexpert_chest14_pretrain/timm-efficientnet-b3_128_pretrain_step0.pth
+rm chexpert_chest14_pretrain/timm-efficientnet-b3_128_pretrain_step1.pth
+rm rsnapneu_pretrain/timm-efficientnet-b3_128_deeplabv3plus_rsnapneu.pth
+rm checkpoints/timm-efficientnet-b3_128_deeplabv3plus_aux_fold0.pth
+
 python train_chexpert_chest14.py --steps 0 1 --cfg $CFG --frac=$FRAC  &&
 
 python train_rsnapneu.py --cfg $CFG --epochs=2 --frac=$FRAC &&
